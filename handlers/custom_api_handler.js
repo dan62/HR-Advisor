14bdafb80ecb4134a74e0679a7f4d21e54146178
dan <danielgoncalves62@gmail.com>
2020-11-18 01:08:04 +0700

    added ability for multiple trees without parents

--- handlers/custom_api_handler.js
+++ handlers/custom_api_handler.js
@@ -28,52 +28,62 @@ Author: Daniel Goncalves
 // Importing custom helper services
 const algorithm_helper = require('../services/algorithm_helper');
 const validation_helper = require('../services/validation_helper');
-const { response } = require('express');
 
 const match_children = async (req, h) => {
 
-    // 1. Requesting payload data and inititializing it to a variable
+    // Requesting payload data and inititializing it to a variable
     let request_body = req.payload;
 
-    let is_json_valid = await validation_helper.is_valid_json(req.payload);
+    // Verifying weather JSON is valid or not
+    let is_json_valid = await validation_helper.is_valid_json(request_body);
 
     if (is_json_valid) {
 
         // Retreiving values from parent object array
-        const values = Object.values(request_body)
+        let values = Object.values(request_body)
 
         // Convert the format of input into a more data freindly format
         let formatted_input = algorithm_helper.format_input(values);
 
-        // Depth First TreeSort Algorithm
-        let tree = formatted_input.find(o => o.parent_id === null);
-
-        console.log(tree)
-
-        // Marking visited nodes
-        var cmp = function (a, b) {
-            if (a.title < b.title) return -1;
-            if (a.title > b.title) return 1;
-            return 0;
-        };
-
-        // Adding children to the tree
-        tree = algorithm_helper.addChildrenToNode(tree, cmp);
-
+        // Declare the resultant array
         let resultant = []
-        resultant.push(tree)
+
+        // Iterating over nodes in input array to identify the trees
+        formatted_input.forEach(node => {
+            if (node.parent_id == null) {
+                // Create a new tree since node does not have parent
+                let tree = node
+                // Marking visited nodes
+                var cmp = function (a, b) {
+                    if (a.title < b.title) return -1;
+                    if (a.title > b.title) return 1;
+                    return 0;
+                };
+                // Adding children to the tree
+                tree = algorithm_helper.addChildrenToNode(tree, cmp);
+                // Push value to resultant array
+                resultant.push(tree)
+            }
+        });
 
         // Returning of modified tree
         return resultant
+
     } else {
-        return {
-            "statusCode": 500, "error": "JSON Invalid", "message": "Please ensure your request contains valid JSON"
+
+        // Errored response in the case that JSON is invalid
+        let response_err = {
+            "statusCode": 500,
+            "error": "JSON Invalid",
+            "message": "Please ensure your request contains valid JSON"
         }
-    }
 
-    };
-
-    // Exporting of module
-    module.exports = {
-        match_children_handler: match_children
+        // Return error message
+        return response_err
     }
+};
+
+// Exporting of module
+module.exports = {
+    match_children_handler: match_children
+}
